42a1f04caf94223b04d5a876f0cbf83ded9b769e
Camila <cvasquezdr7@gmail.com>
2022-04-11 16:23:04 -0400

    handle non-object, defined, inputs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // set result var equal to empty string
+  var result = '';
+  // if object is numbers
+  if (typeof obj === 'number') {
+    // concat toString value
+    result += obj.toString();
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === true) {return 'true';}
+  if (obj === false) {return 'false';}
+
+  // if object is a string then concat to result
+    // return result var
+  if (typeof obj === 'string') {return result.concat('"' + obj + '"');}
+
+  // if object is an array
+  if (Array.isArray(obj)) {
+        // iterate over array
+    for (var i = 0; i < obj.length; i++) {
+      obj[i]
+    }
+  }
+
+      // sent current index thru main function
+  // otherwise, if typeof obj is object
+    // for each key/value? in obj thru main function
+  return result;
 };
+
+'[a, b, c]'
+'{key: value}'
+/*
+turn everyting into a string
+ - strings
+   - concat strings
+ - numbers
+ - objects
+   - typeof object (if array test fails)
+ - arrays
+   - use Array.isArray()
+
+ Tests:
+ - fill in expected result
+*/
